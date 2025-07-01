4244d00fbe1d3a338f063dc12c520518f265ec7b
Phan Minh Khoi <pmkhoi2420@apcs.fitus.edu.vn>
2025-07-01 23:42:24 +0700

    Add Screen class, create MenuScrean and modify related class

--- src/TitleScreen.cpp
+++ src/TitleScreen.cpp
@@ -1,18 +1,23 @@
 #include "TitleScreen.h"
 #include "ResourceManager.h"
 
-TitleScreen::TitleScreen() : 
+TitleScreen::TitleScreen() : Screen(),
     startButton("StartButton", {GetScreenWidth() - 330.0f, GetScreenHeight() - 110.0f} , 0.3f), 
     credit("Credit", {100.0f, GetScreenHeight() - 60.0f}, 0.3f, BLACK, GetFontDefault(), 40.0f),
-    textures(ResourceManager::getInstance().getTextures()) {}
+    temp("TEST BUTTON", "longButton", {50.0f, 100.0f}, 1.0f, RED, GetFontDefault(), 30.0f) {}
 
 void TitleScreen::draw() {
     DrawTexture(textures["title_screen"], 0, 0, WHITE);
     DrawTexture(textures["guiRayMarioLogo"], 10, 10, WHITE);
     startButton.draw();
     credit.draw();
+    temp.draw();
 }
 
-Button& TitleScreen::getStartButton() {
+ButtonTexture& TitleScreen::getStartButton() {
     return startButton;
+}
+
+ButtonText& TitleScreen::getCreditButton() {
+    return credit;
 }
