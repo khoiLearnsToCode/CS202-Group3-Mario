5b91c0b74aeae22fd519e21bd69219baeed6e09b
Phan Minh Khoi <pmkhoi2420@apcs.fitus.edu.vn>
2025-07-01 23:41:02 +0700

    Add Button class for inheritance

--- header/Button.h
+++ header/Button.h
@@ -9,38 +9,62 @@
 #define HOVER_SPEED 10.0f
 #define HOVER_SCALE 1.075f
 
-
 class Button : public virtual Drawable {
+    protected:
     Vector2 pos;
-    Texture2D btnTexture;
-    float btnScale;
-    void update();
     Rectangle hitbox;
+    float btnScale;
+
+    virtual void update() = 0;
 
     public:
         Button() = default;
-        Button(const char* key, Vector2 position, float scale);
-        ~Button();
+        Button(Vector2 position, float scale);
+        virtual ~Button() = default;
+        virtual void draw() = 0;
+        virtual bool isPressed();
+};
+
+
+class ButtonTexture : public virtual Button {
+    Texture2D btnTexture;
+    void update() override;
+
+    public:
+        ButtonTexture() = default;
+        ButtonTexture(const char* key, Vector2 position, float scale);
+        ~ButtonTexture() override;
         void draw() override;
-        bool isPressed();
 
 };
 
-class ButtonText : public virtual Drawable {
-    std::string text;
-    Vector2 pos;
-    Color textColor;
-    float fontSize;
-    float btnScale;
-    Font font;
-    Rectangle hitbox;
-    void update();
+class ButtonText : public virtual Button {
+       
+    virtual void update() override;
 
+    protected:
+        std::string text;
+        Color textColor;
+        float fontSize;
+        Font font;
 
     public:
         ButtonText() = default;
         ButtonText(const std::string& text, Vector2 position, float scale, Color color, Font font, float fontSize);
-        ~ButtonText() = default;
+        virtual ~ButtonText() = default;
+        virtual void draw() override;
+};
+
+class ButtonTextTexture : public ButtonText {
+    Texture2D btnTexture1;
+    Texture2D btnTexture2;
+    Texture2D* currentTexture;
+
+    void update() override;
+
+    public:
+        ButtonTextTexture() = default;
+        ButtonTextTexture(const std::string& text, const char* key, Vector2 position, float scale, Color color, Font font, float fontSize);
+        ~ButtonTextTexture() override;
         void draw() override;
-        bool isPressed() ;
 };
