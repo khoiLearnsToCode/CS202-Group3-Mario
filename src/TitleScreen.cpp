f15d5221a386f20a280824278909e37f914ab3f3
Phan Minh Khoi <pmkhoi2420@apcs.fitus.edu.vn>
2025-07-02 22:42:18 +0700

    Implementing Menu Screen (partially done)

--- src/TitleScreen.cpp
+++ src/TitleScreen.cpp
@@ -4,7 +4,7 @@
 TitleScreen::TitleScreen() : Screen(),
     startButton("StartButton", {GetScreenWidth() - 330.0f, GetScreenHeight() - 110.0f} , 0.3f), 
     credit("Credit", {100.0f, GetScreenHeight() - 60.0f}, 0.3f, BLACK, GetFontDefault(), 40.0f),
-    temp("TEST BUTTON", "longButton", {50.0f, 100.0f}, 1.0f, RED, GetFontDefault(), 30.0f) {}
+    temp("TEST BUTTON", "longButton", {50.0f, 100.0f}, 2.0f, WHITE, ResourceManager::getInstance().getFont("fixedsys"), 40.0f) {}
 
 void TitleScreen::draw() {
     DrawTexture(textures["title_screen"], 0, 0, WHITE);
