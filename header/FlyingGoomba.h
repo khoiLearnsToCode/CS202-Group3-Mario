cbc510f1fe62148982bd9f935ac946dd6f111130
Minh Thuan <minhthuan524568@gmail.com>
2025-08-04 14:55:27 +0700

    fix bug map2 and modify the flight range of flying goomba

--- header/FlyingGoomba.h
+++ header/FlyingGoomba.h
@@ -1,10 +1,16 @@
-#pragma once
+﻿#pragma once
 
 #include "Baddie.h"
 #include "raylib.h"
 
 class FlyingGoomba : public Baddie {
-    
+private:
+    float moveDistance;      // Theo dõi khoảng cách di chuyển ngang
+    float maxMoveDistance;   // Khoảng cách tối đa trước khi đảo ngược hướng
+    float timeAcum;          // Thời gian tích lũy cho chuyển động hình sin
+    float amplitude;         // Biên độ dao động (độ cao lên xuống)
+    float frequency;         // Tần số dao động (tốc độ lên xuống)
+    float initialY;          // Vị trí y ban đầu để làm mốc cho dao động
 public:
 
     FlyingGoomba( Vector2 pos, Vector2 dim, Vector2 vel, Color color );
